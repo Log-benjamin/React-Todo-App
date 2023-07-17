63f1d65f7179676206f4f369e4193acf9cdffd01
binyamT <binyampowerhc@gmail.com>
2023-07-18 01:16:54 +0300

    add the todo app react files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import TodoApp from './components/TodoApp';
+import './styles/app.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <TodoApp />
+  </React.StrictMode>,
 );
